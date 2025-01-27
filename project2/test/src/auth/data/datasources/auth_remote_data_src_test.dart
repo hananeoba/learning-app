4507153b478ea79eea510fe89774434b24894e0d
hananeoba <hananeouldbabaali@gmail.com>
2025-01-27 14:39:07 +0100

    test-test

---
+++ project2/test/src/auth/data/datasources/auth_remote_data_src_test.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:project2/src/auth/data/datasources/auth_remote_data_src.dart';
+
+class MockFirebaseStorage extends Mock implements FirebaseStorage {}
+
+class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+
+class MockFirebaseFireStore extends Mock implements FirebaseFirestore {}
+
+void main() {
+  late FirebaseAuth authClient;
+  late FirebaseFirestore cloudStoreClient;
+  late FirebaseStorage dbClient;
+
+  late AuthRemoteDataSrc dataSrc;
+
+  setUp(() {
+    authClient = MockFirebaseAuth();
+    cloudStoreClient = MockFirebaseFireStore();
+    dbClient = MockFirebaseStorage();
+    dataSrc = AuthRemoteDataSrcImpl(
+      authClient: authClient,
+      cloudStoreClient: cloudStoreClient,
+      datebase: dbClient,
+    );
+  });
+  group('SignµIn', () {
+    test('should complete successfully when call to server is successful ',
+        () async {
+      // arrange
+      when(
+        () => authClient.signInWithEmailAndPassword(
+          email: any(named: 'email'),
+          password: any(named: 'password'),
+        ),
+      ).thenAnswer((_) async => Future.value());
+      // act
+      final result = await dataSrc.signIn(
+        email: any(named: 'email'),
+        password: any(named: 'password'),
+      );
+      // assert
+      expect(result, true);
+    });
+    test('', () {});
+  });
+}
